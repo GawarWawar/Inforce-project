59714a471e3169981e184fb24370dbc522b3ec9e
GawarWawar <gawarwawar@gmail.com>
2024-07-03 02:57:37 +0300

    add postgres database

--- api_server/settings.py
+++ api_server/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'api_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "123qwedsacxz",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
