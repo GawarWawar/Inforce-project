5850aa0cbf7c7e069f8e8a2e90d4a0d27ba86549
GawarWawar <gawarwawar@gmail.com>
2024-07-02 19:58:34 +0300

    changed user to user from employee +added rule: all fields = unique together

--- restaurant_api/models.py
+++ restaurant_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 from user_auth.models import Employee
 
@@ -17,6 +18,9 @@ class Menu(models.Model):
     description = models.CharField("Description", max_length=1000, default=0)
 
 class Vote(models.Model):
-    user = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, verbose_name="Menu of the day", on_delete=models.CASCADE)
     day_of_vote = models.DateField("Day of vote", default=datetime.date.today)
+    
+    class Meta:
+        unique_together = ['user', 'menu', "day_of_vote"]
