e6d37777585b29193bfb437814281349a6e207a0
GawarWawar <gawarwawar@gmail.com>
2024-07-02 12:09:00 +0300

    added user serializer and signup functionality

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,9 +1,25 @@
+from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+
+from .serializers import UserSerializer
+
+
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data["username"])
+        user.set_password(request.data["password"])
+        token = Token.objects.create(user=user)
+        return Response({"token": token.key, "user": serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
