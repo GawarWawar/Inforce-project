9f3304bb7a4adfe331296770bab5529da2b2c8e7
GawarWawar <gawarwawar@gmail.com>
2024-07-02 17:43:33 +0300

    added views for menus

--- restaurant_api/views.py
+++ restaurant_api/views.py
@@ -59,3 +59,44 @@ def restaurants_by_id(request: HttpRequest, restaurant_id):
 
             return Response({"restaurant": particular_restaurant.data, "changed": changed})
 
+@api_view(["GET", "POST"])
+def menus (request: HttpRequest):
+    if request.method == "GET": 
+        all_menus = serialize_all_model_objects(
+            models.Menu,
+            serializers.MenuSerializer
+        )
+        return Response({"menu": all_menus.data})
+    
+    elif request.method == "POST":
+        new_menu = serializers.RestaurantSerializer(data = request.data)
+        if new_menu.is_valid():
+            new_menu.save()
+            
+            return Response({"menu": new_menu.data})
+        else:
+            return Response(new_menu.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(["GET", "PUT"])      
+def menus_by_id(request: HttpRequest, menu_id):
+        if request.method == "GET": 
+            particular_menu = models.Menu.objects.filter(pk = menu_id)
+            
+            particular_menu = serializers.MenuSerializer(particular_menu, many = True)
+            return Response({"menu": particular_menu.data})
+        
+        if request.method == "PUT":
+            particular_menu = get_object_or_404(models.Restaurant, pk = menu_id)
+            
+            changed = False
+            for field in request.data:
+                if particular_menu.__dict__[field] != request.data[field]:
+                    particular_menu.__dict__[field] = request.data[field]
+                    changed = True
+            particular_menu.save()
+
+            particular_menu = serializers.RestaurantSerializer(
+                models.Restaurant.objects.get(pk = menu_id)
+            )
+
+            return Response({"menu": particular_menu.data, "changed": changed})
