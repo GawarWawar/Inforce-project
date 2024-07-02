10161949aa96e836cb9c320ef9c5c55959509913
GawarWawar <gawarwawar@gmail.com>
2024-07-02 15:59:10 +0300

    added apps to settings and runned migrations

--- api_server/settings.py
+++ api_server/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    
+    "restaurant_api.apps.RestaurantApiConfig",
+    "user_auth.apps.UserAuthConfig",
 ]
 
 REST_FRAMEWORK = {
